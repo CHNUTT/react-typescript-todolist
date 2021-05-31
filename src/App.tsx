13d48ab6aca5db83e39fccf3ddb41a7b7e7a5a2b
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-05-31 13:37:00 -0500

    [ Type definition for Task ] - implement ITask interface to define the type for task in external file - interfaces.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,48 @@
-import React, { FC } from 'react';
+import { ChangeEvent, FC, useState } from 'react';
+import { ITask } from './interfaces';
 import './App.css';
 
 const App: FC = () => {
+	const [task, setTask] = useState<string>('');
+	const [deadline, setDeadline] = useState<number>(0);
+	const [todoList, setTodoList] = useState<ITask[]>([]);
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+		if (e.target.name === 'task') {
+			setTask(e.target.value);
+			return;
+		}
+		setDeadline(+e.target.value);
+	};
+
+	const addTask = (): void => {
+		const newTask: ITask = {
+			taskName: task,
+			deadline: deadline,
+		};
+		setTodoList([...todoList, newTask]);
+		setTask('');
+		setDeadline(0);
+	};
+
 	return (
 		<div className='App'>
 			<div className='header'>
 				<div className='inputContainer'>
-					<input type='text' placeholder='Task...' />
-					<input type='number' placeholder='Deadline (in Days)...' />
+					<input
+						name='task'
+						type='text'
+						placeholder='Task...'
+						value={task}
+						onChange={handleChange}
+					/>
+					<input
+						name='deadLine'
+						type='number'
+						placeholder='Deadline (in Days)...'
+						value={deadline}
+						onChange={handleChange}
+					/>
 				</div>
 				<button>Add Task</button>
 			</div>
